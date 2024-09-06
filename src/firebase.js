8c86ec69f0efc443b28fc25cb87e3f97cadf6742
josiahod <115951301+josiahod@users.noreply.github.com>
2024-09-06 14:15:43 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAXBAM-_XeVWAlYcED12mYfxZCrArbTzY8",
-  authDomain: "fitbitchallenge-2dfee.firebaseapp.com",
-  projectId: "fitbitchallenge-2dfee",
-  storageBucket: "fitbitchallenge-2dfee.appspot.com",
-  messagingSenderId: "1048988428808",
-  appId: "1:1048988428808:web:950bb96e268a2eac29ffbd",
-  measurementId: "G-13R6H6MF66"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
